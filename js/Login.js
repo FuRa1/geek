a3896a1f0f45825abcbad59dbe8bc360fd464194
FuRa1 <lalique.surf@gmail.ru>
2016-02-22 11:12:25 +0200

    'last'

--- js/Login.js
+++ js/Login.js
@@ -8,41 +8,42 @@
 
     function Login($http) {
 
-        var reqObj ={};
+        var reqObj = {};
 
-         return function http(userLogin, userPassword, hotpPassword) {
+        return function http(userLogin, userPassword, hotpPassword) {
 
-            var url = 'https://93.183.203.13:10443/login';
+            var parameter, req, url;
 
-            if(userLogin && userPassword){              //check value from input fields, to work from different states,
+            url = 'https://93.183.203.13:10443/login';
+
+            if (userLogin && userPassword) {              //check value from input fields, to work from different states,
                 reqObj.Login = userLogin;               //allow to not lose login && password, and save them in factory
                 reqObj.Password = userPassword;         //when state /hotp
             }
 
-            if(hotpPassword){
+            if (hotpPassword) {
                 reqObj.Hotp = hotpPassword;             //check hotp pass value, for /authentication state
             }                                           //allow to not send Hotp:'undefined' in first request
-            console.log(reqObj);
 
-            var parameter = JSON.stringify(reqObj);     //restrict request data to JSON format
+            parameter = JSON.stringify(reqObj);         //restrict request data to JSON format
             console.log(parameter);
-            var req = {
+            req = {
                 method: 'POST',
                 url: url,
                 dataType: "json",
                 data: parameter,
-                headers:{
-                    'Content-Type': JSON,
+                headers: {
+                    'Content-Type': 'application/json',
                     'Access-Control-Allow-Origin': "*"
                 }
             };
 
             return $http(req
             ).then(function successCallback(response) {
-                return response;
-            }, function errorCallback(response) {
-                console.log(response);
-            });
+                    return response;
+                }, function errorCallback(response) {
+                    console.log(response);
+                });
         }
 
     }
